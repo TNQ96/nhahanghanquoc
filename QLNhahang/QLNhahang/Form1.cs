72d6f2432574c4b9c38c61c0aa4a141c22f49405
TNQ96 <trannhatquang96ver2@gmail.com>
2016-12-06 08:08:53 +0700

    Project ban dau

---
+++ QLNhahang/QLNhahang/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QLNhahang
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btQuanLy_Click(object sender, EventArgs e)
+        {
+            FrmQuanly f = new FrmQuanly();
+            f.Show();
+        }
+    }
+}
